a6bdb738ad4ea50dfc22a3d4f91ae61629a67ebe
Ulukbek-Toichuev <t.ulukbek01@gmail.com>
2025-01-17 01:13:22 +0300

    Refresh .gitignore rules

--- internal/db/migrations/20250115165303_create_users_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-	role TEXT NOT NULL,
-    created_date DATETIME NOT NULL,
-    deleted_date DATETIME
-);
--- internal/model/generic.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type GeneralMessageResponse struct {
-	Message string `json:"message"`
-}
-
-func NewGeneralMessageResponse(mssg string) *GeneralMessageResponse {
-	return &GeneralMessageResponse{mssg}
-}
